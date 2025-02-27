4be3a1f960fd68ecdf5ef355830f0d8c45bd5007
Mikhail <81607649+blurmb@users.noreply.github.com>
2025-02-27 22:07:29 +0300

    init settings

---
+++ src/global.d.ts
@@ -0,0 +1,16 @@
+declare module "*.module.scss" {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+declare module "*.svg" {
+  const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  export default ReactComponent;
+}
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.webp";
+
+declare module "*.woff";
+declare module "*.woff2";
---
+++ webpack.config.js
@@ -0,0 +1,109 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = (_, argv) => {
+  const isDev = argv.mode === "development";
+  return {
+    entry: "./src/index",
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "bundle.js",
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                [
+                  "@babel/preset-env",
+                  {
+                    useBuiltIns: "usage",
+                    corejs: 3,
+                  },
+                ],
+                ["@babel/preset-react", { runtime: "automatic" }],
+                "@babel/preset-typescript",
+              ],
+            },
+          },
+        },
+        {
+          test: /\.(ts|tsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                [
+                  "@babel/preset-env",
+                  {
+                    useBuiltIns: "usage",
+                    corejs: 3,
+                  },
+                ],
+                ["@babel/preset-react", { runtime: "automatic" }],
+                "@babel/preset-typescript",
+              ],
+            },
+          },
+        },
+        {
+          test: /\.s(a|c)ss$/,
+          use: [
+            isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+            {
+              loader: "css-loader",
+              options: {
+                modules: {
+                  auto: /\.module\.(s(a|c)ss)$/,
+                  localIdentName: isDev
+                    ? "[path][name]__[local]--[hash:base64:5]"
+                    : "[hash:base64:5]",
+                  namedExport: false,
+                  exportLocalsConvention: "camel-case-only",
+                },
+                esModule: false,
+              },
+            },
+            "postcss-loader",
+            "sass-loader",
+          ],
+        },
+        {
+          test: /\.svg$/,
+          issuer: /\.[jt]sx?$/,
+          use: ["@svgr/webpack"],
+        },
+
+        {
+          test: /\.(png|jpe?g|gif|woff2|woff)$/i,
+          use: ["file-loader"],
+        },
+      ],
+    },
+    resolve: {
+      alias: {
+        "@src": path.resolve(__dirname, "src/"),
+      },
+      extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./public/index.html",
+      }),
+      new MiniCssExtractPlugin({
+        filename: isDev ? "[name].css" : "[name].[fullhash].css",
+        chunkFilename: isDev ? "[id].css" : "[id].[fullhash].css",
+      }),
+    ],
+    devtool: isDev && "inline-source-map",
+    devServer: {
+      static: "./dist",
+    },
+  };
+};
