062907e0e213e72f85a7b1a0ff58fd078ef42bf9
Mikhail <81607649+blurmb@users.noreply.github.com>
2025-03-02 16:35:53 +0300

    timerangetolabel model->ui slice

--- src/features/TimeRangeSelector/index.ts
+++ src/features/TimeRangeSelector/index.ts
@@ -1,4 +1,4 @@
 export { TimeRangeSelector } from "./ui";
-export { timeRangeSlice, TimeRanges, TimeRangeToLabel } from "./model/slice";
+export { timeRangeSlice, TimeRanges } from "./model/slice";
 export type { TimeRange } from "./model/slice";
 export { getTimeRange } from "./model/selectors";
--- src/features/TimeRangeSelector/ui/index.tsx
+++ src/features/TimeRangeSelector/ui/index.tsx
@@ -3,8 +3,15 @@ import classes from "./TimeRangeSelector.module.scss";
 import classNames from "classnames";
 import { useAppDispatch, useAppSelector } from "@src/store/hooks";
 import { getTimeRange } from "../model/selectors";
-import { setTimeRange, TimeRangeToLabel } from "../model/slice";
+import { setTimeRange, TimeRange } from "../model/slice";
 import { TimeRanges } from "../model/slice";
+
+const TimeRangeToLabel: { [key in TimeRange]: string } = {
+  "24h": "24h",
+  "7d": "7 days",
+  "30d": "30 days",
+  all: "All time",
+};
 
 type TimeRangeSelectorProps = {
   className?: string;
